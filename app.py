0bdecdbd964b5d7aececb8ec9d77076a4572adbb
nicosagas1 <sagasteguinicolas69@gmail.com>
2024-09-09 12:16:05 -0300

    Actualizacion 5

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Ruta para la página de chat
 @app.route('/chat')
 def chat():
     return render_template('chat.html')
 
-
+# Ruta para la página de inicio de sesión
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-
+# Ruta para la página de programas
 @app.route('/programas')
 def programas():
     return render_template('programas.html')
 
-
+# Ruta para la página de tecnología
 @app.route('/tecnologia')
 def tecnologia():
-    return render_template('tecnologia.html')
+    return render_template('tecnolog.html')
 
 
 if __name__ == '__main__':
